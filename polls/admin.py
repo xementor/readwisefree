f3a5facc05256d5a802d85fb2dc6c2701ba949f8
xementor <ahammedzonaid@gmail.com>
2022-07-06 07:18:16 +0600

    vote implemente

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Question, Choice
+from .models import Question, Choice, User
 
 # Register your models here.
+admin.site.register(User)
 admin.site.register(Question)
 admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -5,9 +5,17 @@ from django.db import models
 
 # Create your models here.
 
+class User(models.Model):
+	name = models.CharField(max_length=40)
+	password = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
 class Question(models.Model): #extended Model
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
 	
 	def __str__(self):
 		return self.question_text
@@ -19,6 +27,8 @@ class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.choice_text
+
